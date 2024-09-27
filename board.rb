2bb12275d7f1cf987f80b88dc230f381db45cd6b
Evan Mastro <mastroevan@gmail.com>
2024-09-27 08:48:29 -0700

    connect 4 completed successfully, verified with testing.

--- board.rb
+++ board.rb
@@ -1,8 +1,14 @@
 # board.rb
 class Board
-    attr_reader :grid
-  
-    def initialize
-      @grid = Array.new(6) { Array.new(7, nil) }
-    end
+  attr_reader :grid
+
+  def initialize
+    @grid = Array.new(6) { Array.new(7, nil) }  # A 7x6 grid
   end
+
+  def add_piece(column, symbol)
+    row = @grid.rindex { |r| r[column].nil? }  # Find the lowest empty row in the column
+    raise 'Column full' unless row             # Raise error if the column is full
+    @grid[row][column] = symbol
+  end
+end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,9 +1,21 @@
-require_relative '../board'
-
 # spec/board_spec.rb
+require_relative '../board'  # Require the file that contains the Board class
+
 describe Board do
-    it 'initializes a 7x6 empty board' do
-      board = Board.new
-      expect(board.grid).to eq(Array.new(6) { Array.new(7, nil) })
-    end
+  it 'initializes a 7x6 empty board' do
+    board = Board.new
+    expect(board.grid).to eq(Array.new(6) { Array.new(7, nil) })
   end
+
+  it 'allows a piece to be dropped in a column' do
+    board = Board.new
+    board.add_piece(1, 'X')
+    expect(board.grid[5][1]).to eq('X')  # Expect the piece to drop to the bottom
+  end
+
+  it 'raises an error when a column is full' do
+    board = Board.new
+    6.times { board.add_piece(1, 'X') }  # Fill the column
+    expect { board.add_piece(1, 'X') }.to raise_error('Column full')
+  end
+end
